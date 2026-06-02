a1d31debbed4f449f2d8c30f70a902d179b83ec2
Woochang Jung <137390313+WoochYon@users.noreply.github.com>
2026-06-02 19:35:33 +0900

    Create block.py

---
+++ block.py
@@ -0,0 +1,27 @@
+import torch.nn as nn
+from attention import MultiHeadAttention
+
+class FeedForward(nn.Module):
+    def __init__(self, emb_dim, dropout=0.1):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(emb_dim, 4 * emb_dim),
+            nn.ReLU(),
+            nn.Linear(4 * emb_dim, emb_dim),
+            nn.Dropout(dropout),
+        )
+    def forward(self, x):
+        return self.net(x)
+
+class Block(nn.Module):
+    def __init__(self, emb_dim, num_heads, block_size, dropout=0.1):
+        super().__init__()
+        self.ln1 = nn.LayerNorm(emb_dim)
+        self.sa = MultiHeadAttention(emb_dim, num_heads, block_size, dropout)
+        self.ln2 = nn.LayerNorm(emb_dim)
+        self.ffwd = FeedForward(emb_dim, dropout)
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
