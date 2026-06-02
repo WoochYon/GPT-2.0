7111025be00aa628846adef01e089e88a4abfcfa
Woochang Jung <137390313+WoochYon@users.noreply.github.com>
2026-06-02 19:35:53 +0900

    Create generate.py

---
+++ generate.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn.functional as F
+
+@torch.no_grad()
+def sample_gpt(model, block_size, stoi, itos, device, start_text="Milton Friedman", max_new_tokens=400):
+    model.eval()
+
+    context = torch.zeros((1, block_size), dtype=torch.long, device=device)
+
+    for ch in start_text:
+        if ch in stoi:
+            ix = torch.tensor([[stoi[ch]]], device=device)
+            context = torch.cat([context[:, 1:], ix], dim=1)
+
+    out = list(start_text)
+
+    for _ in range(max_new_tokens):
+        logits = model(context)
+        logits = logits[:, -1, :]
+
+        probs = F.softmax(logits, dim=-1)
+        ix = torch.multinomial(probs, num_samples=1)
+
+        out.append(itos[ix.item()])
+        context = torch.cat([context[:, 1:], ix], dim=1)
+
+    return "".join(out)
