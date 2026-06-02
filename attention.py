6df1a3f748e981f289d01575bc92bd72eb7d5338
Woochang Jung <137390313+WoochYon@users.noreply.github.com>
2026-06-02 19:35:24 +0900

    Create attention.py

---
+++ attention.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Head(nn.Module):
+    def __init__(self, emb_dim, head_size, block_size, dropout=0.1):
+        super().__init__()
+        self.key = nn.Linear(emb_dim, head_size, bias=False)
+        self.query = nn.Linear(emb_dim, head_size, bias=False)
+        self.value = nn.Linear(emb_dim, head_size, bias=False)
+        self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        B, T, C = x.shape
+
+        k = self.key(x)
+        q = self.query(x)
+        v = self.value(x)
+
+        wei = q @ k.transpose(-2, -1) * (k.size(-1) ** -0.5)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float("-inf"))
+        wei = F.softmax(wei, dim=-1)
+        wei = self.dropout(wei)
+
+        out = wei @ v
+        return out
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, emb_dim, num_heads, block_size, dropout=0.1):
+        super().__init__()
+        head_size = emb_dim // num_heads
+        self.heads = nn.ModuleList([Head(emb_dim, head_size, block_size, dropout) for _ in range(num_heads)])
+        self.proj = nn.Linear(emb_dim, emb_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.proj(out)
+        out = self.dropout(out)
+        return out
